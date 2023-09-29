a345e2f9f966d30fe43bdf8a89bdfeb752f80fdf
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-09-29 11:48:34 -0600

    First Redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementByAmount } from './store/slices/counter';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { counter } = useSelector(state => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <>
       <h1>Redux App</h1>
+      <p>Count is: {counter}</p>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <button onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch(incrementByAmount(2))}>
+          Increment by 2
         </button>
       </div>
     </>
